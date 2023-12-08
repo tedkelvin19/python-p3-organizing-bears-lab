ae2d0c93585db52c40a9dda76238408d2e024c1d
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-12-08 17:19:44 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 3, 'M', 'Cinnamon', 'Happy', true),
+('Rowdy', 1, 'M', 'Brown', 'Gentle', true),
+('Tabitha', 2, 'F', 'White', 'Happy', true),
+('Sergeant Brown', 5, 'M', 'Brown', 'Happy', False),
+('Melissa', 9, 'F', 'Blond', 'Protective', true),
+('Grinch', 4, 'M', 'White', 'Playful', true),
+('Wendy', 8, 'F', 'Black', 'Happy', true),
+(NULL, 1, 'M', 'Brown', 'Joyful', false);
